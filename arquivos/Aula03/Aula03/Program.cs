faf64fa4dc042d7dd01c493e9e8d5a2e8158ed3f
Gislaine <gimcjf@gmail.com>
2020-09-19 13:33:10 -0300

    Aprendendo sobre Bloco using

---
+++ arquivos/Aula03/Aula03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Aula03 {
+    class Program {
+        static void Main(string[] args) {
+
+            string path = "C:\\Users\\gimcj\\Documents\\temp\\Curso_De_C-\\arquivos\\file1.txt";
+            try {
+               
+                    using (StreamReader sr = File.OpenText(path)) {
+                        while (!sr.EndOfStream) {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+
+                
+
+
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error ecurred");
+                Console.WriteLine(e.Message);
+            } 
+        }
+    }
+}
