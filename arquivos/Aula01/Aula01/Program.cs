0e918993939887d1d5e9680ee6a25cacce7054b1
Gislaine <gimcjf@gmail.com>
2020-09-19 11:14:28 -0300

    Alterações na pasta aula01 File, FileInfo e IOException

---
+++ arquivos/Aula01/Aula01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Aula01 {
+    class Program {
+        static void Main(string[] args) {
+
+            string sourcePath = "C:\\Users\\gimcj\\Documents\\temp\\ws-vs2019\\Curso_De_C-\\arquivos\\file1.txt";
+            string targetPath = "C:\\Users\\gimcj\\Documents\\temp\\ws-vs2019\\Curso_De_C-\\arquivos\\file2.txt";
+
+            try {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines) {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e) {
+                Console.WriteLine("An erroroccurred ");
+                Console.WriteLine(e.Message);
+
+            }
+
+        }
+    }
+}
