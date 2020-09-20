570af7aaaf0e0617339d5f1ad6eec1e6f72437d4
Gislaine <gimcjf@gmail.com>
2020-09-20 18:53:42 -0300

    progração Funcional e Cálculo Lambda

---
+++ Linq/Aula02/Aula02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula02 {
+    class Program {
+
+        public static int glabalValue = 3;
+        static void Main(string[] args) {
+
+            int[] vect = new int[] {3, 4, 5 };
+            ChangeOddValues(vect);
+            Console.WriteLine(string.Join(" ", vect));
+
+        }
+
+        private static void ChangeOddValues(int[] numbers) {
+            for (int i=0; i< numbers.Length;i++) {
+                if (numbers[i] % 2 != 0) {
+                    numbers[i] += glabalValue;
+                }
+
+            }
+        }
+    }
+}
