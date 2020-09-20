2f07fb5e478c1a7f52945984b8e0fc259e279753
Gislaine <gimcjf@gmail.com>
2020-09-20 09:55:17 -0300

    Generics PARTE 1

---
+++ Generics/Aula01/Aula01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+
+//Problema motivador 1 (reuso)
+
+//Deseja - se fazer um programa que leia um conjunto de N números inteiros (N de 1 a 10), e depois imprima esses números de forma organizada conforme exemplo. Em seguida, informar qual foi o primeiro valor informado.
+
+//EXEMPLO:
+//How many values? 3
+//10
+//8
+//23
+//Saida:
+//[10,8,23]
+//First: 10
+
+namespace Aula01 {
+    class Program {
+        static void Main(string[] args) {
+            PrintService printService = new PrintService();
+
+            Console.WriteLine("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i =0; i<n;i++) {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+            printService.Print();
+            Console.WriteLine("First: "+printService.First());
+        }
+    }
+}
