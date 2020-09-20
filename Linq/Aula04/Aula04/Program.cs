802f48f58f0ae599708fbb0ef9555350df0c1d5a
Gislaine <gimcjf@gmail.com>
2020-09-20 19:12:57 -0300

    Multicast delegates

---
+++ Linq/Aula04/Aula04/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aula04.service;
+using System;
+
+namespace Aula04 {
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program {
+        static void Main(string[] args) {
+
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.showSum;
+            op += CalculationService.showMax;
+
+            op.Invoke(a,b);
+
+        }
+    }
+}
---
+++ Linq/Aula04/Aula04/service/CalculationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula04.service {
+    class CalculationService {
+
+        public static void showMax(double x, double y) {
+            double max = (x > y) ? x : y;
+            Console.WriteLine(max);
+        }
+
+        public static void showSum(double x, double y) {
+            double sum = x + y;
+            Console.WriteLine(sum);
+        }
+
+
+    }
+}
