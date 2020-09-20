6de69e70f0175cb2475f57df8cee6ecfcedfffc0
Gislaine <gimcjf@gmail.com>
2020-09-20 14:22:50 -0300

    hashSet e SortedSet PARTE 1

---
+++ Generics/Aula04/Aula04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula04 {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+            Console.WriteLine(set.Contains("Computer"));
+
+            foreach (string p in set) {
+                Console.WriteLine(p);
+            }
+
+
+        }
+    }
+}
